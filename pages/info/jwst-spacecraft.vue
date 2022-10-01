02292d3d8e50f1059858d78c526df5f4c4a5986a
Emilio Cignetti <emilio@cignetti.ar>
2022-10-01 15:23:41 -0300

    Add more info pages

---
+++ pages/info/jwst-spacecraft.vue
@@ -0,0 +1,57 @@
+<template>
+  <page-container>
+
+    <h1>Telescopio Espacial James Webb (Spacecraft Bus)</h1>
+
+    <p>El Spacecraft bus es el principal elemento de soporte del Telescopio Espacial James Webb (JWST). Alberga una multitud de componentes informáticos, de comunicación, de propulsión y estructurales. Los otros tres elementos del JWST son el elemento del telescopio óptico (OTE), el módulo de instrumentos científicos integrados (ISIM) y el parasol.</p>
+
+    <div  style="display:flex; flex-flow:row wrap; justify-content:center;">
+      <figure>
+        <img src="/img/spacecraft1.png" width="570px" />
+        <figcaption><small>Fig .1: Ubicación del Spacecraft bus en el telescopio. Obsérvese que siempre está expuesto por el Sol.</small></figcaption>
+      </figure>
+    </div>
+    <br>
+
+    <p>El Spacecraft bus debe soportar estructuralmente el telescopio espacial de 6,5 toneladas, con un peso de solo 350 kg. Está hecho principalmente de material compuesto de grafito. Se ensambló en el estado de California en 2015 y luego tuvo que integrarse con el resto del telescopio espacial antes de su lanzamiento.</p>
+
+    <p>El Spacecraft bus está localizado en el lado "cálido" del telescopio que mira hacia el Sol y opera a una temperatura de aproximadamente 300 kelvins (80 °F, 27 °C). Otro aspecto importante del bus de la nave espacial es el equipo central de computación, almacenamiento de memoria y comunicaciones. El procesador y el software dirigen los datos hacia y desde los instrumentos, al núcleo de memoria de estado sólido y al sistema de radio que puede enviar datos a la Tierra y recibir comandos. La computadora también controla el apuntamiento y el movimiento de la nave espacial, tomando datos de los sensores de los giroscopios y el rastreador de estrellas, y enviando los comandos necesarios a las ruedas de reacción o propulsores.</p>
+
+    <div  style="display:flex; flex-flow:row wrap; justify-content:center;">
+      <figure>
+        <img src="/img/spacecraft2.png" width="570px" />
+        <figcaption><small>Fig .2: El Spacecraft Bus dentro del armazón circular de color bronce</small></figcaption>
+      </figure>
+    </div>
+    <br>
+
+    <p>Es una caja de fibra de carbono que alberga una gran cantidad de sistemas importantes que mantienen el telescopio en funcionamiento, como los paneles solares y las computadoras. También contiene el enfriador MIRI y algunos componentes electrónicos ISIM.</p>
+
+    <p>Hay seis subsistemas principales que se encuentran dentro de él:</p>
+
+    <ul>
+      <li>Subsistema de Energía Eléctrica</li>
+      <li>Subsistema de control de altitud</li>
+      <li>Subsistema de comunicación</li>
+      <li>Subsistema de manejo de datos (C&DH)</li>
+      <li>Subsistema de propulsión</li>
+      <li>Subsistema de Control Térmico</li>
+    </ul>
+
+    <p>Además, tiene dos rastreadores de estrellas, seis ruedas de reacción y los sistemas de propulsión (tanque de combustible y propulsores). Las dos tareas principales son apuntar el telescopio y realizar el mantenimiento de la estación para su órbita estable en el punto de Lagrange L2.</p>
+
+    <div id="boton">
+      <button class="boton-personalizado" onclick="window.history.back();">Volver</button></div>
+  </page-container>
+</template>
+
+<script>
+import PageContainer from "~/components/PageContainer";
+
+export default {
+  name: 'JwstSpacecraft',
+  components: [
+    PageContainer
+  ]
+}
+</script>
---
+++ pages/info/jwst-sunshield.vue
@@ -0,0 +1,49 @@
+<template>
+  <page-container>
+    <h1>Telescopio Espacial James Webb (Barrera solar -multilayer sunshield-)</h1>
+
+    <p>Para realizar observaciones en el espectro infrarrojo, el JWST debe mantenerse a una temperatura muy baja, aproximadamente por debajo de 50 K (-220 °C), de lo contrario, la radiación infrarroja del propio telescopio podría bloquear o sobrecargar sus instrumentos.</p>
+
+    <p>Para evitarlo utiliza un gran parasol que bloquea la luz y el calor del Sol, la Tierra y la Luna, además, su posición cercana al punto de Lagrange Tierra-Sol L2 mantiene los tres cuerpos en el mismo lado de la nave espacial en todo momento.</p>
+
+    <p>Su órbita halo alrededor del punto L2 evita la sombra de la Tierra y la Luna, manteniendo una posición constante y aceptable para la barrera solar y los paneles solares. El parasol está hecho de película de poliimida y tiene membranas recubiertas con aluminio en un lado y silicio en el otro.</p>
+
+    <p>El parasol está diseñado para doblarse doce veces, por lo que cabe dentro de la cubierta del cohete Ariane 5 de 4,57 m × 16,19 m. Una vez ubicado el telescopio en el punto L2, el parasol se desplega a 21,197 m × 14,162 m. El parasol fue ensamblado a mano en Man Tech (NeXolve) en Huntsville, Alabama, antes de ser entregado a Northrop Grumman en Redondo Beach, California, Estados Unidos, para su prueba.</p>
+
+    <p>Cada capa está recubierta de aluminio, y el lado que mira al sol de las dos capas más calientes (designadas como Capa 1 y Capa 2) también tienen un recubrimiento de "silicio dopado" (o silicio tratado) para reflejar el calor del sol hacia el espacio. El parasol es una parte fundamental del telescopio Webb porque las cámaras e instrumentos infrarrojos de a bordo deben mantenerse muy fríos y fuera del calor y la luz del sol para funcionar correctamente.</p>
+
+    <p>El kapton es una película de poliamida desarrollada por DuPont a finales de los años sesenta. Tiene una gran resistencia al calor y se mantiene estable en una amplia gama de temperaturas que van de menos 269 a más 400 Celsius (de menos 452 a más 752 grados Fahrenheit). No se funde ni se quema a las temperaturas más altas. En la Tierra, la película de poliimida Kapton puede utilizarse en una gran variedad de aplicaciones de aislamiento eléctrico y electrónico.</p>
+
+    <p>Las capas del parasol también están recubiertas de aluminio y silicio dopado por sus propiedades ópticas y su longevidad en el entorno espacial. El dopaje es un proceso en el que se mezcla una pequeña cantidad de otro material durante el proceso de recubrimiento de silicio para que el recubrimiento sea conductor de la electricidad. El revestimiento debe ser conductor de la electricidad para que las membranas puedan conectarse a tierra con el resto del JWST y no acumulen una carga eléctrica estática en su superficie. El silicio tiene una alta emisividad, lo que significa que emite la mayor parte del calor y la luz y actúa bloqueando el calor del sol para que no llegue a los instrumentos infrarrojos que estarán situados debajo. Además, las superficies de aluminio, muy reflectantes, rebotan la energía restante en los huecos de los bordes del parasol. (Dato curioso: el tono rosado de la parte inferior del parasol se debe en realidad a este revestimiento).</p>
+
+    <div  style="display:flex; flex-flow:row wrap; justify-content:center;">
+      <figure>
+        <img src="/img/parasol1.png" width="570px" />
+        <figcaption><small>Fig .1: Imagen de las capas del parasol del telescopio James Webb</small></figcaption>
+      </figure>
+    </div>
+    <br>
+
+    <div  style="display:flex; flex-flow:row wrap; justify-content:center;">
+      <figure>
+        <img src="/img/parasol2.png" width="570px" />
+        <figcaption><small>Fig .2: Imagen de las capas del parasol del telescopio James Webb</small></figcaption>
+      </figure>
+    </div>
+    <br>
+
+    <div id="boton">
+      <button class="boton-personalizado" onclick="window.history.back();">Volver</button></div>
+  </page-container>
+</template>
+
+<script>
+import PageContainer from "~/components/PageContainer";
+
+export default {
+  name: 'JwstSunshield',
+  components: [
+    PageContainer
+  ]
+}
+</script>
