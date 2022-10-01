bc88e13d478c1bd8ca90c9feb02900c57d6fa4f7
Emilio Cignetti <emilio@cignetti.ar>
2022-10-01 12:47:55 -0300

    First commit

---
+++ components/GridItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <a class="flex flex-col flex-nowrap items-center" :href="href">
+    <div class="w-64 h-64 rounded-2xl overflow-hidden border-2 border-transparent hover:border-red-500">
+      <img :src="image" />
+    </div>
+    <span>
+      {{subtitle}}
+    </span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'GridItem',
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    href: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="px-4 sm:px-64 flex flex-col gap-8">
+    <h1 class="text-2xl text-text-center text-red-500 font-extrabold mt-8">Aprendiendo a través del Espejo</h1>
+
+    <div class="flex flex-wrap items-center gap-16">
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+      <grid-item image="img/placeholder.png" href="puzzle/index.html" subtitle="Puzzles" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="css" scoped>
+</style>
